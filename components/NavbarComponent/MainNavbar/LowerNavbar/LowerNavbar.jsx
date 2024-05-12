2674a8f7c91d28e6890bda4e47eb53152b87a178
Valentine Omonya <valomosh254@gmail.com>
2024-05-12 15:11:06 +0300

    Added navbar

--- components/NavbarComponent/MainNavbar/LowerNavbar/LowerNavbar.jsx
+++ components/NavbarComponent/MainNavbar/LowerNavbar/LowerNavbar.jsx
@@ -51,7 +51,7 @@ const LowerNavbar = () => {
     <div className="h-14 w-full flex justify-center items-center dark:bg-slate-950 bg-gray-50">
       <div className="max-w-[1400px] w-full flex justify-between items-center ">
         <div
-          className="md:hidden pl-6 dark:text-white text-slate-600 font-semibold  hover:text-slate-950 dark:hover:text-slate-300 cursor-pointer "
+          className="lg:hidden pl-6 dark:text-white text-slate-600 font-semibold  hover:text-slate-950 dark:hover:text-slate-300 cursor-pointer "
           onClick={() => toggleSidebar()}
         >
           <TbMenu
@@ -61,26 +61,26 @@ const LowerNavbar = () => {
           <TbX size={36} className={`${showSideMenu ? "hidden" : "block"}`} />
         </div>
         <nav
-          className={`dark:text-white text-slate-600 font-semibold fixed w-1/2 h-full md:static md:w-full md:h-fit md:shadow-none shadow-[26px_-1px_25px_-25px_rgba(204,204,204,0.74)] top-28 transition-all ease-in-out duration-300 ${
+          className={`dark:text-white text-slate-600 font-semibold fixed w-1/2 h-full lg:static lg:w-full lg:h-fit lg:shadow-none shadow-[26px_-1px_25px_-25px_rgba(204,204,204,0.74)] top-28 transition-all ease-in-out duration-300 ${
             showSideMenu ? "left-[-500px]" : "left-0"
           }`}
         >
-          <ul className="flex  flex-start flex-col  gap-x-4 w-full px-2 md:flex-row md:justify-center md:items-center cursor-pointer">
+          <ul className="flex  flex-start flex-col  gap-x-4 w-full px-2 lg:flex-row lg:justify-center lg:items-center cursor-pointer">
             {navbarItems &&
               navbarItems.map((navbarItem, i) => (
                 <li
                   key={i}
-                  className="hover:text-slate-950 dark:hover:text-slate-300 whitespace-nowrap mt-6 md:mt-0"
+                  className="hover:text-slate-950 dark:hover:text-slate-300 whitespace-nowrap mt-6 lg:mt-0"
                 >
                   <Link href={navbarItem.href}>{navbarItem.name}</Link>
                 </li>
               ))}
           </ul>
         </nav>
-        <div className="font-bold text-2xl flex justify-center md:justify-start items-center w-full md:pl-10 text-slate-700 whitespace-nowrap">
+        <div className="font-bold text-2xl flex justify-center lg:justify-start items-center w-full lg:pl-10 text-slate-700 whitespace-nowrap">
           KNECOMPUTER PROJECTS
         </div>
-        <div className="flex justify-center items-center gap-x-2 pr-10 md:pr-0 ">
+        <div className="flex justify-center items-center gap-x-2 pr-10 lg:pr-0 ">
           {iconItems &&
             iconItems.map((iconItem, i) => (
               <div
